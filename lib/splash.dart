53b8d52312111a425e8954539aac9936ce94fb5c
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-09-10 13:09:33 -0700

    Add loading screen

---
+++ lib/splash.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:CoolQuiz/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => HomePage(),
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 35.0,
+        ),
+      ),
+    );
+  }
+}
